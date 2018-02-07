f4e95a17168e852435da209f8ad6fed481bb9d61
Michaela Robosova <robosova.michaela@seznam.cz>
2018-02-07 22:03:59 +0100

    Start using context in TreeNavigation tests

--- src/components/TreeNavigation/__tests__/TreeNavigation.test.js
+++ src/components/TreeNavigation/__tests__/TreeNavigation.test.js
@@ -30,7 +30,7 @@ const items= [
   ]},
 ];
 
-describe('TreeNavigation ', () => {
+describe('TreeNavigation', () => {
   it('renders', () => {
     const wrapper = mount(TreeNavigation, {
       propsData: {
@@ -41,100 +41,114 @@ describe('TreeNavigation ', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it('renders item as a simple value when both `to` and `href` fields are undefined', () => {
-    const wrapper = mount(TreeNavigation, {
-      propsData: {
-        items: [{
-          name: 'Item',
-        }],
-      },
+  describe('item', () => {
+    context('with `to` and `href` fields undefined', () => {
+      it('is rendered as a simple value', () => {
+        const wrapper = mount(TreeNavigation, {
+          propsData: {
+            items: [{
+              name: 'Item',
+            }],
+          },
+        });
+
+        expect(wrapper.find('li').html()).toBe('<li>Item</li>');
+      });
     });
 
-    expect(wrapper.find('li').html()).toBe('<li>Item</li>');
+    context('with `href` field defined', () => {
+      it('is rendered as a hyperlink', () => {
+        const wrapper = mount(TreeNavigation, {
+          propsData: {
+            items: [{
+              name: 'Item',
+              href: '#contact',
+            }],
+          },
+        });
+
+        expect(wrapper.find('li').contains('a')).toBe(true);
+      });
+
+      it('hyperlink is not assigned `router-link` class', () => {
+        const wrapper = mount(TreeNavigation, {
+          propsData: {
+            items: [{
+              name: 'Item',
+              href: '#contact',
+            }],
+          },
+        });
+
+        expect(wrapper.find('a').classes()).not.toContain('router-link');
+      });
+    });
+
+    context('with `to` field defined', () => {
+      it('is rendered as a router link', () => {
+        const wrapper = mount(TreeNavigation, {
+          propsData: {
+            items: [{
+              name: 'Item',
+              to: 'home',
+            }],
+          },
+        });
+
+        expect(wrapper.find('li').contains('router-link')).toBe(true);
+      });
+
+      it('router link is assigned `router-link` class', () => {
+        const wrapper = mount(TreeNavigation, {
+          propsData: {
+            items: [{
+              name: 'Item',
+              to: 'home',
+            }],
+          },
+        });
+
+        expect(wrapper.find('router-link').classes()).toContain('router-link');
+      });
+    });
   });
 
-  it('renders item as a hyperlink when `href` field is defined', () => {
-    const wrapper = mount(TreeNavigation, {
-      propsData: {
-        items: [{
-          name: 'Item',
-          href: '#contact',
-        }],
-      },
+  describe('lists', () => {
+    context('with level deeper than default open level', () => {
+      it('are assigned `closed` class', () => {
+        const wrapper = mount(TreeNavigation, {
+          propsData: {
+            items,
+            defaultOpenLevel: 2,
+          },
+        });
+
+        // level 3 list
+        expect(wrapper.find('div > ul > ul > ul > ul').classes()).toContain('closed');
+
+        // level 4 list
+        expect(wrapper.find('div > ul > ul > ul > ul > ul').classes()).toContain('closed');
+      });
     });
 
-    expect(wrapper.find('li').contains('a')).toBe(true);
-  });
+    context('with level less than or equal to default open level', () => {
+      it('are not assigned `closed` class', () => {
+        const wrapper = mount(TreeNavigation, {
+          propsData: {
+            items,
+            defaultOpenLevel: 2,
+          },
+        });
 
-  it('renders item as a router link when `to` field is defined', () => {
-    const wrapper = mount(TreeNavigation, {
-      propsData: {
-        items: [{
-          name: 'Item',
-          to: 'home',
-        }],
-      },
+        // level 0 list
+        expect(wrapper.find('div > ul').classes()).not.toContain('closed');
+
+        // level 1 list
+        expect(wrapper.find('div > ul > ul').classes()).not.toContain('closed');
+
+        // level 2 list
+        expect(wrapper.find('div > ul > ul').classes()).not.toContain('closed');
+      });
     });
-
-    expect(wrapper.find('li').contains('router-link')).toBe(true);
-  });
-
-  it('does not assign `router-link` class to a simple hyperlink', () => {
-    const wrapper = mount(TreeNavigation, {
-      propsData: {
-        items: [{
-          name: 'Item',
-          href: '#contact',
-        }],
-      },
-    });
-
-    expect(wrapper.find('a').classes()).not.toContain('router-link');
-  });
-
-  it('assigns `router-link` class to a router link', () => {
-    const wrapper = mount(TreeNavigation, {
-      propsData: {
-        items: [{
-          name: 'Item',
-          to: 'home',
-        }],
-      },
-    });
-
-    expect(wrapper.find('router-link').classes()).toContain('router-link');
-  });
-
-  it('assigns `closed` class to lists with level deeper than `defaultOpenLevel`', () => {
-    const wrapper = mount(TreeNavigation, {
-      propsData: {
-        items,
-        defaultOpenLevel: 2,
-      },
-    });
-
-    // level 3 list
-    expect(wrapper.find('div > ul > ul > ul > ul').classes()).toContain('closed');
-
-    // level 4 list
-    expect(wrapper.find('div > ul > ul > ul > ul > ul').classes()).toContain('closed');
-  });
-
-  it('does not assign `closed` class to lists with level less than or equal to `defaultOpenLevel`', () => {
-    const wrapper = mount(TreeNavigation, {
-      propsData: {
-        items,
-        defaultOpenLevel: 2,
-      },
-    });
-
-    // level 0 list
-    expect(wrapper.find('div > ul').classes()).not.toContain('closed');
-
-    // level 1 list
-    expect(wrapper.find('div > ul > ul').classes()).not.toContain('closed');
-
-    // level 2 list
-    expect(wrapper.find('div > ul > ul').classes()).not.toContain('closed');
   });
 });
