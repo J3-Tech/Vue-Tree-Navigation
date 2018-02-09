9998341ad89206d8da7bb0eee7a1f212cb508036
Michaela Robosova <robosova.michaela@seznam.cz>
2018-02-09 14:20:20 +0100

    Polish dev app

--- dev/components/Shoes.vue
+++ dev/components/Shoes.vue
@@ -2,27 +2,8 @@
   <div>
     <h1>Running shoes</h1>
     <ul>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
       <li id="race">Race</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
       <li id="road">Road</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
       <li id="trail">Trail</li>
     </ul>
   </div>
