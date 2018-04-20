edc6a1448913d4f0541cbb7f7cb05ed8026288a4
Michaela Robosova <robosova.michaela@seznam.cz>
2018-04-20 09:33:06 +0200

    NavigationItem: Update specs
    - use router instead of mocks to see actual outputs
    - test assigned classes instead of <router-link>, <a> tags
      (which is also not possible anymore with vue-router)

--- src/components/NavigationItem/NavigationItem.spec.js
+++ src/components/NavigationItem/NavigationItem.spec.js
@@ -1,4 +1,5 @@
-import { shallow } from '@vue/test-utils';
+import { shallow, mount, createLocalVue } from '@vue/test-utils';
+import VueRouter from 'vue-router';
 
 import {
   PATH_TYPE_NONE,
@@ -43,7 +44,7 @@ const externalItem = {
 };
 
 describe('NavigationItem ', () => {
-  context('for a text item', () => {
+  context('for a label item', () => {
     it('renders a span containing the item name', () => {
       const wrapper = shallow(NavigationItem, {
         propsData: {
@@ -56,7 +57,7 @@ describe('NavigationItem ', () => {
   });
 
   context('for an external item', () => {
-    it('renders a hyperlink with a correct value, a location and blank target', () => {
+    it('renders a hyperlink with a correct value, location and blank target', () => {
       const wrapper = shallow(NavigationItem, {
         propsData: {
           item: externalItem,
@@ -67,7 +68,7 @@ describe('NavigationItem ', () => {
     });
   });
 
-  context('with router', () => {
+  context('without router', () => {
     context('for an element item', () => {
       let wrapper;
 
@@ -76,22 +77,19 @@ describe('NavigationItem ', () => {
           propsData: {
             item: elementItem,
           },
-          mocks: {
-            $router: {},
-          },
         });
       });
 
-      it("doesn't render a hyperlink", () => {
-        expect(wrapper.contains('a')).toBe(false);
+      it('renders a hyperlink with a correct location and text', () => {
+        expect(wrapper.html()).toMatchSnapshot();
       });
 
-      it('renders a router link with a correct location and text', () => {
-        expect(wrapper.html()).toMatchSnapshot();
+      it('assigns link class to the hyperlink', () => {
+        expect(wrapper.find('a').classes()).toContain('NavigationItem__link');
       });
     });
 
-    context('for route item', () => {
+    context('for a route item', () => {
       let wrapper;
 
       beforeEach(() => {
@@ -99,60 +97,74 @@ describe('NavigationItem ', () => {
           propsData: {
             item: routeItem,
           },
-          mocks: {
-            $router: {},
-          },
         });
       });
 
-      it("doesn't render a hyperlink", () => {
-        expect(wrapper.contains('a')).toBe(false);
+      it('renders a hyperlink with a correct location and text', () => {
+        expect(wrapper.html()).toMatchSnapshot();
       });
 
-      it('renders a router link with a correct location and text', () => {
-        expect(wrapper.html()).toMatchSnapshot();
+      it('assigns link class to the hyperlink', () => {
+        expect(wrapper.find('a').classes()).toContain('NavigationItem__link');
       });
     });
   });
 
-  context('without router', () => {
-    context('for element item', () => {
+  context('with router', () => {
+    let localVue;
+    let router;
+
+    beforeAll(() => {
+      localVue = createLocalVue();
+      localVue.use(VueRouter);
+      router = new VueRouter({});
+    });
+
+    context('for an element item', () => {
       let wrapper;
 
       beforeEach(() => {
-        wrapper = shallow(NavigationItem, {
+        wrapper = mount(NavigationItem, {
+          localVue,
+          router,
           propsData: {
             item: elementItem,
           },
         });
       });
 
-      it("doesn't render a router link", () => {
-        expect(wrapper.contains('router-link')).toBe(false);
-      });
-
       it('renders a hyperlink with a correct location and text', () => {
         expect(wrapper.html()).toMatchSnapshot();
       });
+
+      it('assigns router-link class to the hyperlink', () => {
+        expect(wrapper.find('a').classes()).toContain(
+          'NavigationItem__router-link'
+        );
+      });
     });
 
-    context('for route item', () => {
+    context('for a route item', () => {
       let wrapper;
 
       beforeEach(() => {
-        wrapper = shallow(NavigationItem, {
+        wrapper = mount(NavigationItem, {
+          localVue,
+          router,
           propsData: {
             item: routeItem,
           },
         });
       });
 
-      it("doesn't render a router link", () => {
-        expect(wrapper.contains('router-link')).toBe(false);
-      });
-
       it('renders a hyperlink with a correct location and text', () => {
         expect(wrapper.html()).toMatchSnapshot();
+      });
+
+      it('assigns router-link class to the hyperlink', () => {
+        expect(wrapper.find('a').classes()).toContain(
+          'NavigationItem__router-link'
+        );
       });
     });
   });
