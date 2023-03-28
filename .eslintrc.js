cfae3e866e5832241f79c3a0e38ab146be14a728
Chellem <dickriven@chellemboyee.com>
2023-03-28 22:16:07 +0400

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true,
-    'cypress/globals': true,
-  },
-  extends: ['standard', 'prettier'],
-  plugins: ['html', 'cypress'],
-  globals: {
-    context: true,
-    jsdom: true,
-  },
-};
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  overrides: [
+    {
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
+      ],
+      'extends': [
+        'plugin:cypress/recommended'
+      ]
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
